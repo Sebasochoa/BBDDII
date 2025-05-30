c1afc8ab96667d2cd94a153829b77d14f87e8569
Sebastian Ochoa <104391592+Sebasochoa@users.noreply.github.com>
2025-05-30 11:54:31 -0500

    Update main.cpp

--- Megatron/main.cpp
+++ Megatron/main.cpp
@@ -10,17 +10,40 @@ namespace fs = std::filesystem;
 
 void Menu()
 {
-    std::cout << "1. Crear Disco Default.\n2. Crear Disco Personalizado.\n3. Cargar Archivo.\n4. Select *.\n5.Salir.\nIngrese la opcion a realizar:";
+    std::cout << "1. Crear Disco Personalizado.\n2. Cargar Archivo.\n3. Select *.\n4. Select <\n5. Salir.\nIngrese la opcion a realizar:";
 }
 
 int main()
 {
     // g++ -o main main.cpp SGBD.cpp Records_Manager.cpp Disco.cpp Bloques.cpp
     SGBD A;
-    A.Create_Disk();
-    //A.Select_Discriminado_Archivo();
-    //A.Buscar_reemplazar();
-    //A.Select_Discriminado_Archivo();
+    int opc = 0;
+    while (opc!= -1)
+    {
+        Menu();
+        std::cin>>opc;
+        switch (opc)
+        {
+        case 1:
+            A.Create_Disk();
+            break;
+        case 2:
+            A.Cargar();
+            break;
+        case 3:
+            A.Select();
+            break;
+        case 4:
+            A.Select_Discriminado_Archivo();
+            break;
+        case 5:
+            opc = -1;
+        
+        default:
+            break;
+        }
+    }
+    
     
     return 0;
 }
